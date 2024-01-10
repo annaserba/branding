8ad2cf079a91f9bb8e7c9b14b60f6bde2aec50d8
Анна Серба <a.serba@superjob.ru>
2024-01-10 04:36:36 +0300

    генерация html

--- packages/example/esbuild.config.js
+++ packages/example/esbuild.config.js
@@ -1,5 +1,6 @@
 import esbuild from 'esbuild'
 import copyStaticFiles from "esbuild-copy-static-files"
+import { htmlPlugin } from 'esbuild-html-plugin'
 
 const name = 'example';
 const enviroments=['desktop','mobile']
@@ -10,7 +11,27 @@ enviroments.forEach(async (env) => {
     bundle: true,
     outfile: `../../public/${name}/${env}/${name}.js`,
     metafile: true,
-    plugins: [
+      plugins: [
+       htmlPlugin({
+        outfile: `index.html`,
+        createHeadElements: () => [
+          `<meta charset="utf-8" />`,
+          `<meta name="viewport" content="width=device-width, initial-scale=1" />`,
+        ],
+        createBodyElements: (outputUrls) =>
+          outputUrls
+            .filter((outputUrl) => outputUrl.endsWith(`.js`))
+            .map((outputUrl) => `
+            <div id='${name}_prod'
+              style="max-width: ${env === "desktop"?"1444":"768"}px;
+              margin:0 auto;
+              width: 100%;
+              position: relative;
+              border: 1px solid #000;
+              overflow: hidden;">
+            </div>
+            <script src="/${name}/${env}${outputUrl}"></script>`),
+      }),
       copyStaticFiles({
         src: `./src/${env}/img`,
         dest: `../../public/${name}/${env}/img`,
--- packages/rgd/esbuild.config.js
+++ packages/rgd/esbuild.config.js
@@ -1,5 +1,6 @@
 import esbuild from 'esbuild'
 import copyStaticFiles from "esbuild-copy-static-files"
+import { htmlPlugin } from 'esbuild-html-plugin'
 
 const name = 'rgd';
 const enviroments=['desktop','mobile']
@@ -11,12 +12,33 @@ enviroments.forEach(async (env) => {
     outfile: `../../public/${name}/${env}/${name}.js`,
     metafile: true,
     plugins: [
+      htmlPlugin({
+        outfile: `index.html`,
+        createHeadElements: (outputUrls) => [
+          `<meta charset="utf-8" />`,
+          `<meta name="viewport" content="width=device-width, initial-scale=1" />`,
+          `<link rel="preload" href="/style.css" />`,
+        ],
+        createBodyElements: (outputUrls) =>
+          outputUrls
+            .filter((outputUrl) => outputUrl.endsWith(`.js`))
+            .map((outputUrl) => `
+            <div id='${name}_prod'
+              style="max-width: ${env === "desktop"?"1444":"768"}px;
+              margin:0 auto;
+              width: 100%;
+              position: relative;
+              border: 1px solid #000;
+              overflow: hidden;">
+            </div>
+            <script src="/${name}/${env}${outputUrl}"></script>`),
+      }),
       copyStaticFiles({
         src: `./src/${env}/img`,
         dest: `../../public/${name}/${env}/img`,
         dereference: true,
       }),
-    ]
+    ],
   });
 })
 
